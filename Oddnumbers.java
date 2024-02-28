227efd680ae9f122928b1d92aa0554e22f4f448a
vinay <143823318+Sarcast1c@users.noreply.github.com>
2024-02-28 23:32:05 +0530

    Add files via upload

---
+++ Oddnumbers.java
@@ -0,0 +1,11 @@
+public class Oddnumbers {
+    public static void main(String[] args) {
+        System.out.println("first 100 odd numbers");
+        int num=100;
+        for(int i=0;i<=num;i++){
+            if(i%2!=0){
+           System.out.println(i);
+            }
+        }
+    }
+}
