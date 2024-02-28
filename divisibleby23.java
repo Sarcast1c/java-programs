0711f2a3f248d474fe6d7c333a08ec9606eab536
vinay <143823318+Sarcast1c@users.noreply.github.com>
2024-02-28 23:43:17 +0530

    Add files via upload

---
+++ divisibleby23.java
@@ -0,0 +1,11 @@
+public class divisibleby23 {
+    public static void main(String[] args) {
+        int num=100;
+        for(int i=0;i<=num;i++){
+            if(i%2==0 && i%3==0){
+               System.out.println(i);
+            }
+        }
+    }
+    
+}
