a10eacd1e6ffee2e2bbb9aa3229afdd7a09389d0
vinay <143823318+Sarcast1c@users.noreply.github.com>
2024-02-28 23:35:13 +0530

    Add files via upload

---
+++ Evennumbers.java
@@ -0,0 +1,11 @@
+public class Evennumbers {
+    public static void main(String[] args) {
+        System.out.println("first 100 even numbers");
+        int num=100;
+        for(int i=1;i<=num;i++){
+            if(i%2==0){
+           System.out.println(i);
+            }
+        }
+    }
+}
