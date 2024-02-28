f67cdbc947c48ec80ad7c861d1b13cea7ae37cc0
vinay <143823318+Sarcast1c@users.noreply.github.com>
2024-02-28 23:38:26 +0530

    Add files via upload

---
+++ Multiplesof3.java
@@ -0,0 +1,10 @@
+public class Multiplesof3 {
+    public static void main(String[] args) {
+        int num=100;
+        for(int i=1;i<=num;i++){
+            if(i%3==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
