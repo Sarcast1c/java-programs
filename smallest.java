deaa34fcc5cbb6ef6bd8918ea6db0e84d514845e
vinay <143823318+Sarcast1c@users.noreply.github.com>
2024-02-28 23:34:28 +0530

    Add files via upload

--- smallest.java
+++ smallest.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class smallest {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan =new Scanner(System.in);
         int num1=scan.nextInt();
         int num2=scan.nextInt();
         int num3=scan.nextInt();
